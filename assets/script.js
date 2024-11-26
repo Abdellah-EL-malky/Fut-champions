760f91c43ba54b2cd62f9b0f37ddcf4aa5fd33ef
Abdellah <abdel.elmalky@gmail.com>
2024-11-26 12:27:02 +0100

    ajout du style de la carte et des formations

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ fetch('data/players.json').then(function(response){
 })
 
 .then(function(obj){
-    console.log(obj)
+    console.log(obj);
 })
 
 .catch(function(error){
@@ -11,4 +11,18 @@ fetch('data/players.json').then(function(response){
     console.error(error);
 });
 
+const formations = {
+    '4-4-2': [
+        [{name: 'CF', index: 0}, {name: 'CF', index: 1}],
+        [{name: 'LMF', index: 2}, {name: 'CMF', index: 3}, {name: 'CMF', index: 4}, {name: 'RMF', index: 5}],
+        [{name: 'LB', index: 6}, {name: 'CB', index: 7}, {name: 'CB', index: 8}, {name: 'RB', index: 9}],
+        [{name: 'GK', index: 10}]
+    ],
+    '4-3-3': [
+        [{name: 'LWF', index: 0}, {name: 'CF', index: 1}, {name: 'RWF', index: 2}],
+        [{name: 'CMF', index: 3}, {name: 'DMF', index: 4}, {name: 'CMF', index: 5}],
+        [{name: 'LB', index: 6}, {name: 'CB', index: 7}, {name: 'CB', index: 8}, {name: 'RB', index: 9}],
+        [{name: 'GK', index: 10}]
+    ],
+};console.log(formations);
 
